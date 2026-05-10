40454888bea3d122f1ab56968d4004483f21748a
neupanesudip90 <neupanesudip90@gmail.com>
2026-05-10 23:57:53 +0545

    fix:add visitors count

--- frontend/src/libs/firebase.ts
+++ frontend/src/libs/firebase.ts
@@ -1,8 +1,6 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-console.log("🔥 Firebase API Key:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
-
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
